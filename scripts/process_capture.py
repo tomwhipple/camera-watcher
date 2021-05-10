2f9978dc3254022531b061a15df288e1d093fa5b
Tom Whipple <mail@tomwhipple.com>
2021-05-10 15:58:10 -0500

    separate events by file

--- scripts/process_capture.py
+++ scripts/process_capture.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
+import json
+
+from pathlib import PurePath
 
 EVENT_LOG_FILE = '/etc/opt/kerberosio/capture/video_event_log.txt'
+EVENT_LOG_DIR = PurePath(EVENT_LOG_FILE).parent
 
 def main():
     capture_data = sys.argv[1:]
@@ -13,6 +17,15 @@ def main():
 
     event_log.close()
     
+    parsed_event = json.loads(capture_data[0])
+
+    event_name = PurePath(parsed_event['pathToVideo']).stem
+    event_jsonl_file = str(EVENT_LOG_DIR.joinpath(event_name + '.jsonl'))
+    with open(event_jsonl_file, 'a+') as single_event_log:
+        single_event_log.write(json.dumps(parsed_event, sort_keys=True))
+        single_event_log.write("\n")
+
+    single_event_log.close()
 
 if __name__ == "__main__":
     main()
