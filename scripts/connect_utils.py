5b2c55fcc0e2a0cf7e9aefdf090806d98fc2db8d
Tom Whipple <mail@tomwhipple.com>
2021-12-06 20:05:50 -0600

    make db connection work over ssh tunnel

---
+++ scripts/connect_utils.py
@@ -0,0 +1,226 @@
+import os
+import sys
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from sshtunnel import SSHTunnelForwarder
+
+def get_ssh_tunnel():
+	db_user = os.environ["DB_USER"]
+	db_pass = os.environ["DB_PASS"]
+	db_name = os.environ["DB_NAME"]
+	db_host = os.environ["DB_HOST"]
+	
+	db_ssh_host = os.environ["DB_SSH_HOST"]
+	db_ssh_user = os.environ["DB_SSH_USER"]
+	db_ssh_private_key_file = os.environ.get("DB_SSH_PRIVATE_KEY_FILE", "~/.ssh/id_rsa")
+
+	# Extract port from db_host if present,
+	# otherwise use DB_PORT environment variable.
+	host_args = db_host.split(":")
+	if len(host_args) == 1:
+		db_hostname = host_args[0]
+		db_port = int(os.environ.get("DB_PORT",3306))
+	elif len(host_args) == 2:
+		db_hostname, db_port = host_args[0], int(host_args[1])
+
+	tunnel = SSHTunnelForwarder(
+			(db_ssh_host,22),
+			ssh_username=db_ssh_user,
+			ssh_pkey=db_ssh_private_key_file,
+			remote_bind_address=('127.0.0.1',db_port),
+			local_bind_address=('127.0.0.1',db_port),
+		)
+	return tunnel
+
+def init_connection_engine(ssh_tunnel=None):
+	db_config = {
+		# [START cloud_sql_mysql_sqlalchemy_limit]
+		# Pool size is the maximum number of permanent connections to keep.
+		"pool_size": 5,
+		# Temporarily exceeds the set pool_size if no connections are available.
+		"max_overflow": 2,
+		# The total number of concurrent connections for your application will be
+		# a total of pool_size and max_overflow.
+		# [END cloud_sql_mysql_sqlalchemy_limit]
+
+		# [START cloud_sql_mysql_sqlalchemy_backoff]
+		# SQLAlchemy automatically uses delays between failed connection attempts,
+		# but provides no arguments for configuration.
+		# [END cloud_sql_mysql_sqlalchemy_backoff]
+
+		# [START cloud_sql_mysql_sqlalchemy_timeout]
+		# 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+		# new connection from the pool. After the specified amount of time, an
+		# exception will be thrown.
+		"pool_timeout": 30,  # 30 seconds
+		# [END cloud_sql_mysql_sqlalchemy_timeout]
+
+		# [START cloud_sql_mysql_sqlalchemy_lifetime]
+		# 'pool_recycle' is the maximum number of seconds a connection can persist.
+		# Connections that live longer than the specified amount of time will be
+		# reestablished
+		"pool_recycle": 1800,  # 30 minutes
+		# [END cloud_sql_mysql_sqlalchemy_lifetime]
+
+	}
+
+	if ssh_tunnel:
+		os.environ["DB_HOST"] = ssh_tunnel.local_bind_address[0]
+
+	if os.environ.get("DB_HOST"):
+		if os.environ.get("DB_ROOT_CERT"):
+			return init_tcp_sslcerts_connection_engine(db_config)
+		return init_tcp_connection_engine(db_config)
+	return init_unix_connection_engine(db_config)
+
+# def init_ssh_connection_engine(db_config):
+# 	db_user = os.environ["DB_USER"]
+# 	db_pass = os.environ["DB_PASS"]
+# 	db_name = os.environ["DB_NAME"]
+# 	db_host = os.environ["DB_HOST"]
+	
+# 	db_ssh_host = os.environ["DB_SSH_HOST"]
+# 	db_ssh_user = os.environ["DB_SSH_USER"]
+# 	db_ssh_private_key_file = os.environ.get("DB_SSH_PRIVATE_KEY_FILE", "~/.ssh/id_rsa")
+
+# 	# Extract port from db_host if present,
+# 	# otherwise use DB_PORT environment variable.
+# 	host_args = db_host.split(":")
+# 	if len(host_args) == 1:
+# 		db_hostname = host_args[0]
+# 		db_port = int(os.environ.get("DB_PORT",3306))
+# 	elif len(host_args) == 2:
+# 		db_hostname, db_port = host_args[0], int(host_args[1])
+
+# 	tunnel = SSHTunnelForwarder(
+# 			(db_ssh_host,22),
+# 			ssh_username=db_ssh_user,
+# 			ssh_pkey=db_ssh_private_key_file,
+# 			remote_bind_address=('127.0.0.1',db_port),
+# 			local_bind_address=('127.0.0.1',db_port),
+# 		)
+
+# 	tunnel.start()
+# 	return init_tcp_connection_engine(db_config)
+
+
+def init_tcp_sslcerts_connection_engine(db_config):
+	# [START cloud_sql_mysql_sqlalchemy_create_tcp_sslcerts]
+	# Remember - storing secrets in plaintext is potentially unsafe. Consider using
+	# something like https://cloud.google.com/secret-manager/docs/overview to help keep
+	# secrets secret.
+	db_user = os.environ["DB_USER"]
+	db_pass = os.environ["DB_PASS"]
+	db_name = os.environ["DB_NAME"]
+	db_host = os.environ["DB_HOST"]
+	db_root_cert = os.environ["DB_ROOT_CERT"]
+	db_cert = os.environ["DB_CERT"]
+	db_key = os.environ["DB_KEY"]
+
+	# Extract port from db_host if present,
+	# otherwise use DB_PORT environment variable.
+	host_args = db_host.split(":")
+	if len(host_args) == 1:
+		db_hostname = host_args[0]
+		db_port = int(os.environ.get("DB_PORT",3306))
+	elif len(host_args) == 2:
+		db_hostname, db_port = host_args[0], int(host_args[1])
+
+	ssl_args = {
+		"ssl_ca": db_root_cert,
+		"ssl_cert": db_cert,
+		"ssl_key": db_key
+	}
+
+	pool = sqlalchemy.create_engine(
+		# Equivalent URL:
+		# mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+		sqlalchemy.engine.url.URL.create(
+			#drivername="mysql+pymysql",
+			drivername="mysql",
+			username=db_user,  # e.g. "my-database-user"
+			password=db_pass,  # e.g. "my-database-password"
+			host=db_hostname,  # e.g. "127.0.0.1"
+			port=db_port,  # e.g. 3306
+			database=db_name  # e.g. "my-database-name"
+		),
+		connect_args={"ssl":ssl_args},
+		**db_config
+	)
+	# [END cloud_sql_mysql_sqlalchemy_create_tcp_sslcerts]
+
+	return pool
+
+
+def init_tcp_connection_engine(db_config):
+	# [START cloud_sql_mysql_sqlalchemy_create_tcp]
+	# Remember - storing secrets in plaintext is potentially unsafe. Consider using
+	# something like https://cloud.google.com/secret-manager/docs/overview to help keep
+	# secrets secret.
+	db_user = os.environ["DB_USER"]
+	db_pass = os.environ["DB_PASS"]
+	db_name = os.environ["DB_NAME"]
+	db_host = os.environ["DB_HOST"]
+
+	# Extract port from db_host if present,
+	# otherwise use DB_PORT environment variable.
+	host_args = db_host.split(":")
+	if len(host_args) == 1:
+		db_hostname = db_host
+		db_port = os.environ.get("DB_PORT",3306)
+	elif len(host_args) == 2:
+		db_hostname, db_port = host_args[0], int(host_args[1])
+
+	pool = sqlalchemy.create_engine(
+		# Equivalent URL:
+		# mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+		sqlalchemy.engine.url.URL.create(
+			#drivername="mysql+pymysql",
+			drivername="mysql",
+			username=db_user,  # e.g. "my-database-user"
+			password=db_pass,  # e.g. "my-database-password"
+			host=db_hostname,  # e.g. "127.0.0.1"
+			port=db_port,  # e.g. 3306
+			database=db_name,  # e.g. "my-database-name"
+		),
+		**db_config
+	)
+	# [END cloud_sql_mysql_sqlalchemy_create_tcp]
+
+	return pool
+
+
+def init_unix_connection_engine(db_config):
+	# [START cloud_sql_mysql_sqlalchemy_create_socket]
+	# Remember - storing secrets in plaintext is potentially unsafe. Consider using
+	# something like https://cloud.google.com/secret-manager/docs/overview to help keep
+	# secrets secret.
+	db_user = os.environ["DB_USER"]
+	db_pass = os.environ["DB_PASS"]
+	db_name = os.environ["DB_NAME"]
+	db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+	instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
+
+	pool = sqlalchemy.create_engine(
+		# Equivalent URL:
+		# mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+		sqlalchemy.engine.url.URL.create(
+			#drivername="mysql+pymysql",
+			drivername="mysql",
+			username=db_user,  # e.g. "my-database-user"
+			password=db_pass,  # e.g. "my-database-password"
+			database=db_name,  # e.g. "my-database-name"
+			query={
+				"unix_socket": "{}/{}".format(
+					db_socket_dir,  # e.g. "/cloudsql"
+					instance_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+			}
+		),
+		**db_config
+	)
+	# [END cloud_sql_mysql_sqlalchemy_create_socket]
+
+	return pool
--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import json
 
 import sqlalchemy
@@ -6,18 +8,25 @@ from sqlalchemy import text
 import watcher_model
 from watcher_model import EventObservation
 
+from sshtunnel import SSHTunnelForwarder
+
+import connect_utils
+
 query_uncategorized_sql = "select * from event_observations obs where obs.id not in (select distinct observation_id from event_classifications) limit 5"
 
 def get_uncategorized(request):
-	db = watcher_model.init_connection_engine()
-
-	session = sqlalchemy.orm.Session(db)
-
-	observations = session.query(EventObservation).from_statement(text(query_uncategorized_sql));
-
 	obs_out = []
-	for o in observations:
-		#import pdb; pdb.set_trace()
-		obs_out.append({'video_file': o.video_file, 'capture_time': str(o.capture_time)})
+
+	#import pdb; pdb.set_trace()
+	with connect_utils.get_ssh_tunnel() as tunnel, connect_utils.init_connection_engine(tunnel).begin() as connection:
+		session = sqlalchemy.orm.Session(connection)
+		observations = session.query(EventObservation).from_statement(text(query_uncategorized_sql))
+	
+		for o in observations:
+			obs_out.append({'video_file': o.video_file, 'capture_time': str(o.capture_time)})
 
 	return json.dumps(obs_out, indent=2)
+
+
+if __name__ == "__main__":
+	print(get_uncategorized(None))
