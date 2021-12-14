c3e0f0cd76a141c7c519dd951deb8e5e64f04c62
Tom Whipple <mail@tomwhipple.com>
2021-12-14 10:12:37 -0600

    upload directly to db

--- scripts/process.sh
+++ scripts/process.sh
@@ -2,4 +2,6 @@
 
 # Why can't kerberos launch a Python script?!?
 
-/etc/opt/kerberosio/scripts/record_kerberos_metadata.py $@
+#/etc/opt/kerberosio/scripts/record_kerberos_metadata.py $@
+
+/etc/opt/kerberosio/scripts/watcher.py record_kerberos -d /etc/opt/kerberosio/capture/jsonl $@
--- scripts/watcher.py
+++ scripts/watcher.py
@@ -7,13 +7,16 @@ import pathlib
 import os
 
 from datetime import datetime
+from pathlib import PurePath
 
 import sqlalchemy
+from sqlalchemy import select
+
 import watcher_model
 from watcher_model import EventObservation
-
 from connect_utils import TunneledConnection
 
+DEFAULT_WORKING_DIR = PurePath('/etc/opt/kerberosio/capture/')
 
 def upload_event_in_file(session, filename):
     with open(filename) as fp:
@@ -41,7 +44,31 @@ def upload_events_in_directory(session, basedir, limit):
     for file in filelist:
         upload_event_in_file(session, file)
 
-def record_kerberos_event(session, jsonl_directory):
+
+def write_event_jsonl(parsed_event, working_directory=DEFAULT_WORKING_DIR):
+
+    event_name = PurePath(parsed_event['pathToVideo']).stem
+
+    event_jsonl_file = str(PurePath(working_directory).joinpath(event_name + '.jsonl'))
+    with open(event_jsonl_file, 'a+') as single_event_log:
+        single_event_log.write(json.dumps(parsed_event, sort_keys=True))
+        single_event_log.write("\n")
+
+    single_event_log.close()
+
+def record_kerberos_event(session, input_json, jsonl_directory=DEFAULT_WORKING_DIR):
+    parsed_event = json.loads(input_json)
+
+    stmt = select(EventObservation).where(EventObservation.video_file)
+    result = session.execute(stmt)
+    if len(result.all()) == 0:
+        session.add(EventObservation(
+            video_file=parsed_event['pathToVideo'],
+            scene_name=parsed_event['instanceName'],
+            capture_time=datetime.fromtimestamp(int(parsed_event['timestamp'])),
+            storage_local=True
+        ))
+    write_event_jsonl(parsed_event, jsonl_directory)
 
 
 def main():
@@ -50,6 +77,7 @@ def main():
     parser.add_argument('-d', '--input_directory', type=pathlib.Path)
     parser.add_argument('-f', '--file', type=pathlib.Path)
     parser.add_argument('-l', '--limit', type=int)
+    parser.add_argument('raw_json', nargs='*')
 
     args = parser.parse_args()
 
@@ -60,6 +88,8 @@ def main():
             upload_event_in_file(session, str(args.file))
         elif args.action == 'upload_dir':
             upload_events_in_directory(session, str(args.input_directory), args.limit)
+        elif args.action == 'record_kerberos':
+            record_kerberos_event(session, args.raw_json, args.input_directory)
         elif args.action == 'debug':
             import pdb; pdb.set_trace()
         else:
